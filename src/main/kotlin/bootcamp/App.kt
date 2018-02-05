8b5ed79143c4563634cb11e6e570cdcd47776941
joeldudleyr3 <joel.dudley@r3.com>
2018-02-05 18:05:36 +0000

    Updates readme. Switches to a token example.

--- src/main/kotlin/bootcamp/App.kt
+++ src/main/kotlin/bootcamp/App.kt
@@ -1,7 +1,11 @@
 package bootcamp
 
+import bootcamp.TokenContract.Companion.TOKEN_CONTRACT_ID
+import bootcamp.TokenContract.Issue
 import co.paralleluniverse.fibers.Suspendable
-import net.corda.core.contracts.*
+import net.corda.core.contracts.CommandData
+import net.corda.core.contracts.Contract
+import net.corda.core.contracts.ContractState
 import net.corda.core.flows.FinalityFlow
 import net.corda.core.flows.FlowLogic
 import net.corda.core.flows.InitiatingFlow
@@ -10,21 +14,19 @@ import net.corda.core.identity.Party
 import net.corda.core.transactions.LedgerTransaction
 import net.corda.core.transactions.TransactionBuilder
 import net.corda.core.utilities.ProgressTracker
-import bootcamp.DonationContract.Donate
-import bootcamp.DonationContract.Companion.DONATION_CONTRACT_ID
 
 /* Our state, defining a shared fact on the ledger. */
-data class DonationState(val donor: Party, val charity: Party, val amount: Int) : ContractState {
-    override val participants = listOf(donor, charity)
+data class TokenState(val issuer: Party, val recipient: Party, val amount: Int) : ContractState {
+    override val participants = listOf(issuer, recipient)
 }
 
 /* Our contract, governing how our state will evolve over time. */
-class DonationContract : Contract {
+class TokenContract : Contract {
     companion object {
-        val DONATION_CONTRACT_ID = "bootcamp.DonationContract"
+        val TOKEN_CONTRACT_ID = "bootcamp.TokenContract"
     }
 
-    object Donate: CommandData
+    object Issue: CommandData
 
     override fun verify(tx: LedgerTransaction) {
         if (tx.inputStates.size != 0) throw Exception("Transaction should have no inputs")
@@ -32,19 +34,19 @@ class DonationContract : Contract {
         if (tx.commands.size != 1) throw Exception("Transaction should have one command")
 
         val outputState = tx.outputStates[0]
-        if (outputState !is DonationState) throw Exception("Output should be a DonationState")
-        if (outputState.amount < 0) throw Exception("Donation should be positive")
+        if (outputState !is TokenState) throw Exception("Output should be a TokenState")
+        if (outputState.amount < 0) throw Exception("Token amount should be positive")
 
         val command = tx.commands[0]
-        if (command.value !is Donate) throw Exception("Command should be Donate")
-        if (outputState.donor.owningKey !in command.signers) throw Exception( "Donor must sign the donation")
+        if (command.value !is Issue) throw Exception("Command should be Donate")
+        if (outputState.issuer.owningKey !in command.signers) throw Exception( "Donor must sign the issuance")
     }
 }
 
 /* Our flow, automating the process of updating the ledger. */
 @InitiatingFlow
 @StartableByRPC
-class DonationFlow(val charity: Party, val amount: Int) : FlowLogic<Unit>() {
+class TokenFlow(val charity: Party, val amount: Int) : FlowLogic<Unit>() {
     override val progressTracker = ProgressTracker()
 
     @Suspendable
@@ -54,8 +56,8 @@ class DonationFlow(val charity: Party, val amount: Int) : FlowLogic<Unit>() {
 
         // We build our transaction.
         val txBuilder = TransactionBuilder(notary)
-        txBuilder.addOutputState(DonationState(ourIdentity, charity, amount), DONATION_CONTRACT_ID)
-        txBuilder.addCommand(Donate, ourIdentity.owningKey)
+        txBuilder.addOutputState(TokenState(ourIdentity, charity, amount), TOKEN_CONTRACT_ID)
+        txBuilder.addCommand(Issue, ourIdentity.owningKey)
 
         // We check our transaction is valid based on its contracts.
         txBuilder.verify(serviceHub)
