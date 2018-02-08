78aeaccfecea8b1f089197096baeb147456ad006
joeldudleyr3 <joel.dudley@r3.com>
2018-02-08 10:37:49 +0000

    Adds TokenState tests.

--- src/test/kotlin/bootcamp/StateTests.kt
+++ src/test/kotlin/bootcamp/StateTests.kt
@@ -1,10 +1,25 @@
 package bootcamp
 
+import net.corda.core.contracts.ContractState
+import net.corda.testing.ALICE
+import net.corda.testing.BOB
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class StateTests {
     @Test
-    fun `unwritten test`() {
-        TODO()
+    fun `TokenState has issuer, recipient and amount fields of the correct type`() {
+        TokenState(ALICE, BOB, 1)
+    }
+
+    @Test
+    fun `TokenState implements ContractState`() {
+        assert(TokenState(ALICE, BOB, 1) is ContractState)
+    }
+
+    @Test
+    fun `TokenState has two participants, the issuer and the recipient`() {
+        val tokenState = TokenState(ALICE, BOB, 1)
+        assertEquals(setOf(ALICE, BOB), tokenState.participants.toSet())
     }
 }
