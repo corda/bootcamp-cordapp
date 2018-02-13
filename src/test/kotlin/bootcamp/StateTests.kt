531de42136ae6a0c2d621bf5693902c9bc36b4c5
joeldudley <joeldudley@outlook.com>
2018-02-13 12:42:37 +0000

    Migrates to Corda V3. Adds support for offline version.

--- src/test/kotlin/bootcamp/StateTests.kt
+++ src/test/kotlin/bootcamp/StateTests.kt
@@ -1,25 +1,28 @@
 package bootcamp
 
 import net.corda.core.contracts.ContractState
-import net.corda.testing.ALICE
-import net.corda.testing.BOB
+import net.corda.core.identity.CordaX500Name
+import net.corda.testing.core.TestIdentity
 import org.junit.Test
 import kotlin.test.assertEquals
 
 class StateTests {
+    private val alice = TestIdentity(CordaX500Name("Alice", "", "GB")).party
+    private val bob = TestIdentity(CordaX500Name("Bob", "", "GB")).party
+
     @Test
     fun `TokenState has issuer, recipient and amount fields of the correct type`() {
-        TokenState(ALICE, BOB, 1)
+        TokenState(alice, bob, 1)
     }
 
     @Test
     fun `TokenState implements ContractState`() {
-        assert(TokenState(ALICE, BOB, 1) is ContractState)
+        assert(TokenState(alice, bob, 1) is ContractState)
     }
 
     @Test
     fun `TokenState has two participants, the issuer and the recipient`() {
-        val tokenState = TokenState(ALICE, BOB, 1)
-        assertEquals(setOf(ALICE, BOB), tokenState.participants.toSet())
+        val tokenState = TokenState(alice, bob, 1)
+        assertEquals(setOf(alice, bob), tokenState.participants.toSet())
     }
 }
