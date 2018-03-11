945e9c05494f879f318d66cbc83f98dbb18e3550
joeldudleyr3 <joel.dudley@r3.com>
2018-03-11 09:54:23 +0900

    Removes solutions.

--- java/src/main/java/bootcamp/TokenFlow.java
+++ java/src/main/java/bootcamp/TokenFlow.java
@@ -7,51 +7,11 @@ import net.corda.core.transactions.SignedTransaction;
 import net.corda.core.transactions.TransactionBuilder;
 import net.corda.core.utilities.ProgressTracker;
 
-///* Our flow, automating the process of updating the ledger. */
-// * See src/main/java/examples/IAmAFlowPair.java for an example. */
-//@InitiatingFlow
-//@StartableByRPC
-//public class TokenFlow extends FlowLogic<Void> {
-//    private final ProgressTracker progressTracker = new ProgressTracker();
-//    private final Party recipient;
-//    private final int amount;
-//
-//    public TokenFlow(Party recipient, int amount) {
-//        this.recipient = recipient;
-//        this.amount = amount;
-//    }
-//
-//    @Override
-//    public ProgressTracker getProgressTracker() {
-//        return progressTracker;
-//    }
-//
-//    @Suspendable
-//    @Override
-//    public Void call() throws FlowException {
-//        // We choose our transaction's notary (the notary prevents double-spends).
-//        Party notary = getServiceHub().getNetworkMapCache().getNotaryIdentities().get(0);
-//
-//        // We build our transaction.
-//        TransactionBuilder transactionBuilder = null; // TODO: Build a valid transaction.
-//
-//        // We check our transaction is valid based on its contracts.
-//        transactionBuilder.verify(getServiceHub());
-//
-//        // We sign the transaction with our public key, making it immutable.
-//        SignedTransaction signedTransaction = getServiceHub().signInitialTransaction(transactionBuilder);
-//
-//        // We get the transaction notarised and recorded automatically by the platform.
-//        subFlow(new FinalityFlow(signedTransaction));
-//
-//        return null;
-//    }
-//}
-
-/* SOLUTION */
+/* Our flow, automating the process of updating the ledger.
+ * See src/main/java/examples/IAmAFlowPair.java for an example. */
 @InitiatingFlow
 @StartableByRPC
-public class TokenFlow extends FlowLogic<SignedTransaction> {
+public class TokenFlow extends FlowLogic<Void> {
     private final ProgressTracker progressTracker = new ProgressTracker();
     private final Party recipient;
     private final int amount;
@@ -68,14 +28,12 @@ public class TokenFlow extends FlowLogic<SignedTransaction> {
 
     @Suspendable
     @Override
-    public SignedTransaction call() throws FlowException {
+    public Void call() throws FlowException {
         // We choose our transaction's notary (the notary prevents double-spends).
         Party notary = getServiceHub().getNetworkMapCache().getNotaryIdentities().get(0);
 
         // We build our transaction.
-        TransactionBuilder transactionBuilder = new TransactionBuilder(notary);
-        transactionBuilder.addOutputState(new TokenState(getOurIdentity(), recipient, amount), TokenContract.ID);
-        transactionBuilder.addCommand(new TokenContract.Issue(), getOurIdentity().getOwningKey());
+        TransactionBuilder transactionBuilder = null; // TODO: Build a valid transaction.
 
         // We check our transaction is valid based on its contracts.
         transactionBuilder.verify(getServiceHub());
@@ -84,6 +42,8 @@ public class TokenFlow extends FlowLogic<SignedTransaction> {
         SignedTransaction signedTransaction = getServiceHub().signInitialTransaction(transactionBuilder);
 
         // We get the transaction notarised and recorded automatically by the platform.
-        return subFlow(new FinalityFlow(signedTransaction));
+        subFlow(new FinalityFlow(signedTransaction));
+
+        return null;
     }
 }
--- java/src/main/java/bootcamp/TokenState.java
+++ java/src/main/java/bootcamp/TokenState.java
@@ -7,39 +7,9 @@ import net.corda.core.identity.Party;
 
 import java.util.List;
 
-///* Our state, defining a shared fact on the ledger.
-// * See src/main/kotlin/examples/IAmAState.java and
-// * src/main/kotlin/examples/IAmAlsoAState.java for examples. */
-//public class TokenState {
-//
-//}
+/* Our state, defining a shared fact on the ledger.
+ * See src/main/kotlin/examples/IAmAState.java and
+ * src/main/kotlin/examples/IAmAlsoAState.java for examples. */
+public class TokenState {
 
-/* SOLUTION */
-public class TokenState implements ContractState {
-    private final Party issuer;
-    private final Party recipient;
-    private final int amount;
-
-    public Party getIssuer() {
-        return issuer;
-    }
-
-    public Party getRecipient() {
-        return recipient;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public TokenState(Party issuer, Party recipient, int amount) {
-        this.issuer = issuer;
-        this.recipient = recipient;
-        this.amount = amount;
-    }
-
-    @Override
-    public List<AbstractParty> getParticipants() {
-        return ImmutableList.of(issuer, recipient);
-    }
 }
