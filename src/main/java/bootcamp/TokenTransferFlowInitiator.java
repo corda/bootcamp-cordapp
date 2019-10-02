a4e49f26eb2e27da0f6bbeaed95c0b3d7553602b
Anthony Nixon <anixon604@gmail.com>
2019-10-02 10:08:28 +0100

    added throw exception to flow logic restricting transfer proposal to owner - this is a design decision since owner still must sign anyways

--- src/main/java/bootcamp/TokenTransferFlowInitiator.java
+++ src/main/java/bootcamp/TokenTransferFlowInitiator.java
@@ -1,7 +1,11 @@
 package bootcamp;
 
+import co.paralleluniverse.fibers.Suspendable;
 import net.corda.core.contracts.CommandData;
 import net.corda.core.contracts.StateAndRef;
+import net.corda.core.contracts.StateRef;
+import net.corda.core.contracts.TransactionResolutionException;
+import net.corda.core.crypto.SecureHash;
 import net.corda.core.flows.*;
 import net.corda.core.identity.Party;
 import net.corda.core.transactions.SignedTransaction;
@@ -10,17 +14,24 @@ import net.corda.core.utilities.ProgressTracker;
 
 import java.util.Arrays;
 
-import static java.util.Collections.singletonList;
-
 @InitiatingFlow
 @StartableByRPC
 public class TokenTransferFlowInitiator extends FlowLogic<SignedTransaction> {
-    private final StateAndRef<TokenState> token;
+    private StateAndRef<TokenState> token;
     private final Party target;
     private final int amount;
+    private final String txHash;
 
     public TokenTransferFlowInitiator(StateAndRef<TokenState> token, Party target, int amount) {
         this.token = token;
+        this.target = target;
+        this.amount = amount;
+        this.txHash = null;
+    }
+    // overload for running from shell
+    public TokenTransferFlowInitiator(String txHash, Party target, int amount) throws TransactionResolutionException {
+        this.token = null;
+        this.txHash = txHash;
         this.target = target;
         this.amount = amount;
     }
@@ -32,16 +43,24 @@ public class TokenTransferFlowInitiator extends FlowLogic<SignedTransaction> {
         return progressTracker;
     }
 
+    @Suspendable
     @Override
     public SignedTransaction call() throws FlowException {
+        if (token == null) {
+            SecureHash hash = SecureHash.parse(txHash);
+            token = getServiceHub().toStateAndRef(new StateRef(hash, 0));
+        }
 
         Party notary = getServiceHub().getNetworkMapCache().getNotaryIdentities().get(0);
         Party owner = getOurIdentity();
         Party issuer = token.getState().getData().getIssuer();
         int tokenValue = token.getState().getData().getAmount();
 
+        // FLOW CHECKS
         // Amount must not exceed value in token
         if (amount > tokenValue) throw new FlowException("Amount higher than token value");
+        // Token must be owned by party wishing to propose transaction
+        if (!token.getState().getData().getOwner().equals(owner)) throw new FlowException("Party doesn't own token being transferred");
 
         CommandData commandData = new TokenContract.Commands.Transfer();
 
@@ -54,11 +73,12 @@ public class TokenTransferFlowInitiator extends FlowLogic<SignedTransaction> {
         }
         transactionBuilder.verify(getServiceHub());
 
-        FlowSession session = initiateFlow(owner);
+        FlowSession targetSession = initiateFlow(target); // target counter-party
+        FlowSession issuerSession = initiateFlow(issuer); // original issuer
 
         SignedTransaction signedTransaction = getServiceHub().signInitialTransaction(transactionBuilder);
-        SignedTransaction fullySignedTransaction = subFlow(new CollectSignaturesFlow(signedTransaction, singletonList(session)));
+        SignedTransaction fullySignedTransaction = subFlow(new CollectSignaturesFlow(signedTransaction, Arrays.asList(issuerSession, targetSession)));
 
-        return subFlow(new FinalityFlow(fullySignedTransaction, singletonList(session)));
+        return subFlow(new FinalityFlow(fullySignedTransaction, Arrays.asList(issuerSession, targetSession)));
     }
 }
--- src/test/java/bootcamp/TransferFlowTests.java
+++ src/test/java/bootcamp/TransferFlowTests.java
@@ -2,6 +2,7 @@ package bootcamp;
 
 import net.corda.core.concurrent.CordaFuture;
 import net.corda.core.contracts.Command;
+import net.corda.core.contracts.StateAndRef;
 import net.corda.core.contracts.TransactionState;
 import net.corda.core.transactions.SignedTransaction;
 import net.corda.testing.node.MockNetwork;
@@ -13,6 +14,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Collections;
+import java.util.concurrent.ExecutionException;
 
 import static org.junit.Assert.*;
 
@@ -20,9 +22,11 @@ public class TransferFlowTests {
     private MockNetwork network;
     private StartedMockNode nodeA;
     private StartedMockNode nodeB;
+    private StartedMockNode nodeC;
+    private StateAndRef<TokenState> inputTokenStateRef;
 
     @Before
-    public void setup() {
+    public void setup() throws Exception {
         network = new MockNetwork(
                 new MockNetworkParameters(
                         Collections.singletonList(TestCordapp.findCordapp("bootcamp"))
@@ -30,7 +34,15 @@ public class TransferFlowTests {
         );
         nodeA = network.createPartyNode(null);
         nodeB = network.createPartyNode(null);
+        nodeC = network.createPartyNode(null);
+
+        // issue a token on nodeA
+        TokenIssueFlowInitiator flow = new TokenIssueFlowInitiator(nodeB.getInfo().getLegalIdentities().get(0), 80);
+        CordaFuture<SignedTransaction> future = nodeA.startFlow(flow);
         network.runNetwork();
+
+        SignedTransaction tokenIssueTx = future.get(); // transaction from issuance
+        inputTokenStateRef = tokenIssueTx.getTx().outRef(0);
     }
 
     @After
@@ -38,5 +50,116 @@ public class TransferFlowTests {
         network.stopNodes();
     }
 
+    @Test
+    public void transactionConstructedByFlowUsesTheCorrectNotary() throws Exception {
+        TokenTransferFlowInitiator flow = new TokenTransferFlowInitiator(inputTokenStateRef, nodeC.getInfo().getLegalIdentities().get(0), 80);
+        CordaFuture<SignedTransaction> future = nodeB.startFlow(flow);
+        network.runNetwork();
+        SignedTransaction signedTransaction = future.get();
 
+        TransactionState output = signedTransaction.getTx().getOutputs().get(0);
+
+        assertEquals(network.getNotaryNodes().get(0).getInfo().getLegalIdentities().get(0), output.getNotary());
+    }
+
+    @Test
+    public void transactionConstructedByFlowWithOneOutputHasCorrectIssuerAmountAndOwner() throws Exception {
+        // one output
+        TokenTransferFlowInitiator flow = new TokenTransferFlowInitiator(inputTokenStateRef, nodeC.getInfo().getLegalIdentities().get(0), 80);
+        CordaFuture<SignedTransaction> future = nodeB.startFlow(flow);
+        network.runNetwork();
+        SignedTransaction signedTransaction = future.get();
+
+        assertEquals(1, signedTransaction.getTx().getOutputs().size()); // assert one output
+        TokenState inputToken = inputTokenStateRef.getState().getData();
+        TokenState output1 = (TokenState) signedTransaction.getTx().getOutput(0);
+
+        assertEquals(inputToken.getIssuer(), output1.getIssuer());
+        assertEquals(inputToken.getAmount(), output1.getAmount());
+        assertEquals(nodeC.getInfo().getLegalIdentities().get(0), output1.getOwner());
+    }
+
+    @Test
+    public void transactionConstructedByFlowWithTwoOutputHasCorrectIssuerAmountAndOwner() throws Exception {
+        int transferAmount = 60;
+        // two output
+        TokenTransferFlowInitiator flow = new TokenTransferFlowInitiator(inputTokenStateRef, nodeC.getInfo().getLegalIdentities().get(0), transferAmount);
+        CordaFuture<SignedTransaction> future = nodeB.startFlow(flow);
+        network.runNetwork();
+        SignedTransaction signedTransaction = future.get();
+
+        assertEquals(2, signedTransaction.getTx().getOutputs().size()); // assert two outputs
+        TokenState inputToken = inputTokenStateRef.getState().getData();
+        int inputAmount = inputToken.getAmount();
+        TokenState output1 = (TokenState) signedTransaction.getTx().getOutput(0);
+        TokenState output2 = (TokenState) signedTransaction.getTx().getOutput(1);
+
+        assertEquals(inputToken.getIssuer(), output1.getIssuer());
+        assertEquals(transferAmount, output1.getAmount());
+        assertEquals(nodeC.getInfo().getLegalIdentities().get(0), output1.getOwner()); // transfer amount goes to nodeC
+
+        assertEquals(inputToken.getIssuer(), output2.getIssuer());
+        assertEquals(inputToken.getAmount() - transferAmount, output2.getAmount());
+        assertEquals(nodeB.getInfo().getLegalIdentities().get(0), output2.getOwner()); // leftover goes to nodeB
+    }
+
+    @Test
+    public void transactionConstructedByFlowHasOutputUsingCorrectContract() throws Exception {
+        TokenTransferFlowInitiator flow = new TokenTransferFlowInitiator(inputTokenStateRef, nodeC.getInfo().getLegalIdentities().get(0), 80);
+        CordaFuture<SignedTransaction> future = nodeB.startFlow(flow);
+        network.runNetwork();
+        SignedTransaction signedTransaction = future.get();
+
+        TransactionState output = signedTransaction.getTx().getOutputs().get(0);
+
+        assertEquals("bootcamp.TokenContract", output.getContract());
+    }
+
+    @Test
+    public void transactionConstructedByFlowHasOneTransferCommand() throws Exception {
+        TokenTransferFlowInitiator flow = new TokenTransferFlowInitiator(inputTokenStateRef, nodeC.getInfo().getLegalIdentities().get(0), 80);
+        CordaFuture<SignedTransaction> future = nodeB.startFlow(flow);
+        network.runNetwork();
+        SignedTransaction signedTransaction = future.get();
+
+        assertEquals(1, signedTransaction.getTx().getCommands().size());
+        Command command = signedTransaction.getTx().getCommands().get(0);
+
+        assert (command.getValue() instanceof TokenContract.Commands.Transfer);
+    }
+
+    @Test
+    public void transactionConstructedByFlowHasOneCommandWithIssuerOwnerAndTargetAsSigners() throws Exception {
+        TokenTransferFlowInitiator flow = new TokenTransferFlowInitiator(inputTokenStateRef, nodeC.getInfo().getLegalIdentities().get(0), 80);
+        CordaFuture<SignedTransaction> future = nodeB.startFlow(flow);
+        network.runNetwork();
+        SignedTransaction signedTransaction = future.get();
+
+        assertEquals(1, signedTransaction.getTx().getCommands().size());
+        Command command = signedTransaction.getTx().getCommands().get(0);
+
+        assertEquals(3, command.getSigners().size());
+        assertTrue(command.getSigners().contains(nodeA.getInfo().getLegalIdentities().get(0).getOwningKey())); // issuer
+        assertTrue(command.getSigners().contains(nodeB.getInfo().getLegalIdentities().get(0).getOwningKey())); // owner
+        assertTrue(command.getSigners().contains(nodeC.getInfo().getLegalIdentities().get(0).getOwningKey())); // target
+    }
+
+    @Test
+    public void transactionConstructedByFlowHasNoAttachmentsOrTimeWindows() throws Exception {
+        TokenTransferFlowInitiator flow = new TokenTransferFlowInitiator(inputTokenStateRef, nodeC.getInfo().getLegalIdentities().get(0), 80);
+        CordaFuture<SignedTransaction> future = nodeB.startFlow(flow);
+        network.runNetwork();
+        SignedTransaction signedTransaction = future.get();
+
+        assertEquals(1, signedTransaction.getTx().getAttachments().size());
+        assertNull(signedTransaction.getTx().getTimeWindow());
+    }
+
+    @Test
+    public void transactionConstructedViaStringConstructorWorks() throws Exception {
+        TokenTransferFlowInitiator flow = new TokenTransferFlowInitiator(inputTokenStateRef.getRef().getTxhash().toString(), nodeC.getInfo().getLegalIdentities().get(0), 80);
+        CordaFuture<SignedTransaction> future = nodeB.startFlow(flow);
+        network.runNetwork();
+        SignedTransaction signedTransaction = future.get();
+    }
 }
