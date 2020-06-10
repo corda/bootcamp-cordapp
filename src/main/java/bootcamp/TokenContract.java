e4f483d0e7daa5b214671f68e249ef389524412f
David Awad <me@davidawad.com>
2020-06-10 17:42:07 -0400

    add imports / boilerplate to TokenContract.java

--- src/main/java/bootcamp/TokenContract.java
+++ src/main/java/bootcamp/TokenContract.java
@@ -1,12 +1,20 @@
 package bootcamp;
 
 import net.corda.core.contracts.CommandData;
+import static net.corda.core.contracts.ContractsDSL.requireSingleCommand;    
+import static net.corda.core.contracts.ContractsDSL.requireThat;
+
+import java.util.List;
 
 /* Our contract, governing how our state will evolve over time.
  * See src/main/java/examples/ArtContract.java for an example. */
 public class TokenContract {
     public static String ID = "bootcamp.TokenContract";
 
+
+    public void verify(LedgerTransaction tx) throws IllegalArgumentException {
+    
+    }
 
 
     public interface Commands extends CommandData {
